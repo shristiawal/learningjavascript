5eab49203c31f6878ebbba2d511af9a0ff84e439
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-01 13:16:29 +0545

    concatination,Array literals

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("hello world for shristi");
+
+
+x = "The answer is " + 42; // "The answer is 42"
+y = 42 + " is the answer"; // "42 is the answer"
+z = "37" + 7; // "377"
+console.log(x);
+console.log(y);
+console.log(z);
+
+
+const coffees = ["French Roast", "Colombian", "Kona"];
+console.log(coffees);
+console.log(coffees[2]);
+
+
+const fish = ["Lion", , "Angel"];
+console.log(fish);
+
+const myList = ["home", , "school", ,];
+console.log(myList);
