6fee2927092db1f2a51b9940ed7341854989fc03
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-01 16:03:35 +0545

    switch case

--- loops.js
+++ loops.js
@@ -1,7 +1,25 @@
-
+//if else statement
 let a =1;
 if( a==1){
     console.log("true");
 }else{
     console.log("false");
 }
+
+
+
+//switch case
+
+switch (n){
+    case 1:
+        console.log("its first case");
+
+        break;
+    case 2:
+        console.log("its second case");
+        break;
+       
+     default:
+        console.log("its last case");
+}
+var n=1;
