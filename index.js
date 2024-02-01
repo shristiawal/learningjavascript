98a6a1ece6ec2e560446403272ede15bbef2da4c
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-01 15:23:51 +0545

    basics

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log("hello world for shristi");
 
-
+//concatination
 x = "The answer is " + 42; // "The answer is 42"
 y = 42 + " is the answer"; // "42 is the answer"
 z = "37" + 7; // "377"
@@ -8,7 +8,7 @@ console.log(x);
 console.log(y);
 console.log(z);
 
-
+//Array literals
 const coffees = ["French Roast", "Colombian", "Kona"];
 console.log(coffees);
 console.log(coffees[2]);
@@ -19,3 +19,42 @@ console.log(fish);
 
 const myList = ["home", , "school", ,];
 console.log(myList);
+
+//object literals
+const sales = "Toyota";
+
+function carTypes(name) {
+  return name === "Honda" ? name : `Sorry, we don't sell ${name}.`;
+}
+
+const car = { myCar: "Saturn", getCar: carTypes("Honda"), special: sales };
+
+console.log(car.myCar); // Saturn
+console.log(car.getCar); // Honda
+console.log(car.special); // Toyota
+
+
+const vehicle = { manyCars: { a: "Saab", b: "Jeep" }, 9: "Mazda" };
+
+console.log(vehicle.manyCars.b); // Jeep
+console.log(vehicle[9]); 
+
+//Template literals 
+const cat = 'bella';
+console.log (`hello ${cat}`)
+
+
+//Escaping characters
+const quote = "This plant\"is so beautiful\"planted by me 5 years ago.";
+console.log (quote);
+//broken across multiple lines
+const Quote = "This plant \
+is so beautiful \
+planted by me 5 years ago.";
+console.log(Quote);
+
+
+
+
+
+  
---
+++ loops.js
@@ -0,0 +1,7 @@
+
+let a =1;
+if( a==1){
+    console.log("true");
+}else{
+    console.log("false");
+}
