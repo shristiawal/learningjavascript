955e70885c66ac37cd5ed0e5df98cbdb5b4a4eb9
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-02 12:49:27 +0545

    method , arrow function

---
+++ Method.js
@@ -0,0 +1,8 @@
+const a=10;
+const b=20;
+const obj={
+    add(x,y){
+        return x+y;
+    },
+};
+console.log(obj.add(a,b));
---
+++ arrowfunction.js
@@ -0,0 +1,5 @@
+const a =14;
+const b =15;
+
+const add =(a,b)=>a+b;
+console.log(add(a,b));
