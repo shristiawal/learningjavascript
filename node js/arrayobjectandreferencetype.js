8ae1870692183c94879583eea97b191f6f823d29
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-09 11:49:56 +0545

    arrayobjectandreferencetype

---
+++ node js/arrayobjectandreferencetype.js
@@ -0,0 +1,16 @@
+const person ={
+    name: 'shristi',
+    age:20,
+    greet(){
+        console.log('Hi, i am ' + this .name);
+
+    }
+
+  };
+  person.greet();
+  const hobbies =['Sports','çooking'];
+  hobbies.push('programming');
+  console.log(...hobbies);//... is spread operator
+
+  const copiedArray =[hobbies];
+  console.log(copiedArray);
