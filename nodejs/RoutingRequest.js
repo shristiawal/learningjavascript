d46f55399de2acf4b3137d3282d0896d10f1d974
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-14 18:54:20 +0545

    routing request

--- nodejs/RoutingRequest.js
+++ nodejs/RoutingRequest.js
@@ -6,16 +6,10 @@ const server= http.createServer((req,res)=>{
 
 res.write('<html>');
 res.write('<head><title>Enter message</title></head>');
-res.write('<body><from action ="/message method = "POST"><input type ="text" name= "message"><button type = "submit">send</button></body>')
+res.write('<body><from action ="/message method = "POST"><input type ="text" name= "message"><button type = "submit">send</button>"</body>')
 res.write('</html>');
 res.end();
    }
 
-res.setHeader('content-type','text/html');
-res.write('<html>');
-res.write('<head><title>first page</title></head>');
-res.write('<body><h1>hello frome my node js server!</h1></body>')
-res.write('</html>');
-res.end();
+
 }); 
-server.listen(4000);
