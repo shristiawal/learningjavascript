a2c1ef37d202f7343ca503c4e2d10b634a2d9b03
shristiawal <102451898+shristiawal@users.noreply.github.com>
2024-02-21 12:27:48 +0545

    middleware

---
+++ express/app.js
@@ -0,0 +1,19 @@
+const http =require('http');
+
+const express =require ('express');
+
+const app = express();
+
+app.use ((req, res, next)=>{
+    console.log("in the middleware");
+    next();
+});
+app.use((req,res,next)=>{
+    console.log('in another middleware');
+    res.send('<h1>hello from express !!</h1>');
+
+})
+ 
+const server =http.createServer(app);
+
+app.listen(3000);
